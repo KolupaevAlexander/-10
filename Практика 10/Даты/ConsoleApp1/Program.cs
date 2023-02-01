ef4e80f11858aa96fb6e481b5d796478b70a9de4
KolupaevAlexander <113934423+KolupaevAlexander@users.noreply.github.com>
2023-02-01 10:52:47 +0500

    Add files via upload

---
+++ Практика 10/Даты/ConsoleApp1/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //Задания 1-3
+        static void PrintDoubleArray(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                    Console.Write($"{array[j, i]} ");
+                Console.WriteLine();
+            }
+        }
+        static void Task1()
+        {
+            try
+            {
+                Console.Write("Введите кол-во сотрудников ");
+                int workerCount = Int32.Parse(Console.ReadLine());
+                Console.Write("Введите кол-во рабочих месяцов ");
+                int mounthCount = Int32.Parse(Console.ReadLine());
+                double[,] salary = new double[mounthCount, workerCount];
+                DateTime[] birthdays = new DateTime[workerCount];
+
+                for (int i = 0; i < mounthCount; i++)
+                    for (int j = 0; j < workerCount; j++)
+                    {
+                        Console.Write($"Введите зарплату {j + 1} сотрудника за {i + 1} месяц ");
+                        salary[i, j] = Double.Parse(Console.ReadLine());
+                    }
+
+                for (int i = 0; i < birthdays.Length; i++)
+                {
+                    Console.Write($"Введите дату рождения {i + 1} сотрудника ");
+                    birthdays[i] = DateTime.Parse(Console.ReadLine());
+                }
+                Console.Write("Вывод в виде таблицы ");
+                PrintDoubleArray(salary);
+
+                //Задание 2
+                Console.Write("Наибольшее значение заработной платы работника №2");
+                Console.WriteLine(Task2(salary));
+            }
+            catch
+            {
+                Console.Write("Неверный формат данных ");
+            }
+        }
+        static double Task2(double[,] array)
+        {
+            double max = array[0, 1];
+            for (int i = 1; i < array.GetLength(0); i++)
+                if (max < array[i, 1]) max = array[i, 1];
+            return max; 
+        }
+
+        //Задание 4
+        static void Task4()
+        {
+            try
+            {
+                Console.WriteLine("Введите год");
+                int year = Int16.Parse(Console.ReadLine());
+                DateTime date = new DateTime(year, 1, 1);
+                int Count = 0;
+                while (date.Year == year)
+                {
+                    if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        Count++;
+                    }
+                    date = date.AddDays(1.0);
+                }
+                Console.WriteLine("Кол-во рабочих дней в " + year + " году: {0} ", DateTime.IsLeapYear(year) ? 366 - Count : 365 - Count);
+                Console.WriteLine("Кол-во выходных в " + year + " году: {0} ", Count);
+            }
+            catch
+            {
+                Console.Write("Неверный формат данных ");
+            }
+        }
+
+        //Задание 5
+        static void Task5()
+        {
+            try
+            {
+                Console.Write("Введите возраст ");
+                uint year = UInt32.Parse(Console.ReadLine());
+
+                if (9 < year % 100 && year % 100 < 16)
+                    Console.Write($"{year} лет");
+                else
+                    switch (year % 10)
+                    {
+                        case 1:
+                            Console.Write($"{year} год");
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            Console.Write($"{year} года");
+                            break;
+                        default:
+                            Console.Write($"{year} лет");
+                            break;
+                    }
+            }
+            catch
+            {
+                Console.Write("Неверный формат данных ");
+            }
+        }
+
+        //Задание 6
+        static void Task6()
+        {
+            try
+            {
+                string[] mounths = new string[] { "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+                Console.Write("Введите число ");
+                int day = Int32.Parse(Console.ReadLine());
+                Console.Write("Введите месяц ");
+                int mounth = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{day} {mounths[mounth - 1]}");
+            }
+            catch
+            {
+                Console.Write("Неверный формат данных ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Задания 1-3
+            //Task1();
+
+            //Задание 4
+            //Task4();
+
+            //Задание 5
+            //Task5();
+
+            //Задание 6
+            //Task6();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практика 10/Двумерные массивы. Вариант 1 и 14/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {   
+        //Задание 1
+        static void PrintArray(int[,] array)//Вывод массива в консоль
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            { 
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write($"{array[i, j]} ");
+                Console.WriteLine();
+            }
+        }
+        static int FindMax(int[,] array)//Поиск максимального элемента в двумерном массиве
+        {
+            int max = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    if (max < array[i, j]) max = array[i,j];
+
+            return max;
+        }
+        static int GetSum(int[,] array)//Вычисление суммы элементов двумерного массива
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    sum+=array[i,j];
+
+            return sum;
+        }
+        static int GetMultiply(int[,] array)//Вычисление произведения элементов двумерного массива
+        {
+            int multiply = 1;
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    multiply *= array[i, j];
+
+            return multiply;
+        }
+        static void Task1()
+        {
+            int[,] array = new int[4, 3];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    if (i < 3) array[i, j] = i + 1;
+                    else array[i, j] = 3;
+
+            Console.WriteLine($"Полученный массив");
+            PrintArray(array);
+
+            Console.WriteLine($"\nЗадания по данному массиву");
+            Console.WriteLine($"Максимальный элемент массива - {FindMax(array)}");
+            Console.WriteLine($"Сумма элементов массива - {GetSum(array)}");
+            Console.WriteLine($"Произведение элементов массива - {GetMultiply(array)}");
+        }
+
+        //Задание 14
+        static int[,] FillTrain(int carriages, int places)
+        {
+            int[,] tickets = new int[carriages, places];
+            Random rand = new Random();
+            for (int i = 0; i < carriages; i++)
+                for (int j = 0; j < places; j++)
+                    tickets[i, j] = rand.Next(0, 2);
+            return tickets;
+        }
+
+        static int CountFreePlaces(int[,] tickets, int numberOfCarriage, int places)
+        {
+            int counter = 0;
+            for (int i = 0; i < places; i++)
+                if (tickets[numberOfCarriage-1, i] == 0) counter++;
+            return counter;
+        }
+        static void Task14()
+        {
+            try
+            {
+                int carriages = 10;
+                int places = 36;
+                int[,] tickets = FillTrain(carriages, places);
+                Console.WriteLine("Случайно сгенерированный случай");
+                PrintArray(tickets);
+
+                Console.WriteLine("Введите номер вагона");
+                int number = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Кол-во свободных мест {CountFreePlaces(tickets, number, places)}");
+            }
+            catch 
+            {
+                Console.WriteLine("Неверный ввод");
+            }
+            
+
+        }
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task14();
+
+            Console.ReadLine();
+        }
+    }
+}
